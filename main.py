51d003f6627bcaa0e85a34d6b01aae82a4fdac3d
RyzhkevichMaria <m2401207@edu.misis.ru>
2025-12-28 23:41:41 +0300

    Create main.py
    Add main.py with simple functions

---
+++ main.py
@@ -0,0 +1,13 @@
+def add(a: int, b: int) -> int:
+    """Складывает два числа."""
+    return a + b
+
+
+def multiply(a: int, b: int) -> int:
+    """Умножает два числа."""
+    return a * b
+
+
+if __name__ == "__main__":
+    print("5 + 3 =", add(5, 3))
+    print("4 * 7 =", multiply(4, 7))
